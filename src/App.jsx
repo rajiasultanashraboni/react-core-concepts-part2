39a834eebb3fcf15e7c7385b36cb987abab32d14
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2025-03-04 12:42:44 +0600

    react core concepts part 2 is here

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Counter from './counter'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h3>React Core concepts</h3>
+      <Counter></Counter>
+      
+    </>
+  )
+}
+
+export default App
